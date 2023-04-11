daa77ec272d0f26c224c58e25c427bfe17425850
orpd0523 <orpd0523@gmail.com>
2023-04-11 16:21:17 -0500

    changed divs to buttons and working on logic for calculator buttons

--- scripts.js
+++ scripts.js
@@ -1,1 +1,48 @@
-console.log("I am connected!")
+// console.log("I am connected!");
+
+//screen
+const calculator = {
+  startScreen: "0",
+  firstOperand: null,
+  operatorWaiting: false,
+  secondOperand: null,
+};
+
+//update display
+const updateScreen = () => {
+  const answer = document.querySelector(".calculator__screen");
+  answer.value = calculator.displayValue;
+};
+
+updateScreen();
+//handle key press
+const keys = document.querySelector("#key");
+keys.addEventListener('click', (e)=>{
+    const { target } = e;
+
+    if(!target.matches('button')){
+        return;
+    }
+
+    if (target.classList.contains('operator')){
+        console.log("operator", target.value)
+    return;
+    }
+    
+    if (target.classList.contains("decimal")){
+        console.log('decimal', target.value)
+        return;
+    }
+
+    if (target.classList.contains('clear')){
+        console.log('clear', target.value)
+        return;
+    }
+    console.log('digit', target.value)
+})
+
+const { target } = e;
+
+//input digits
+
+//input decimal point
